895ef8b2735b01c747efdabfb2d1445c51cf8b12
alvaro <asalinero9@gmail.com>
2014-07-17 17:44:35 +0200

    Creando un creador de menús horizontales.
    Issue: https://github.com/alvarosalineromendez/CSS-3-Generator/issues/2

--- CSS-3-Generator/public_html/js/operations.js
+++ CSS-3-Generator/public_html/js/operations.js
@@ -11,30 +11,36 @@ $(document).ready(function() {
         switch (this.firstChild.data) {
             case "Button generator":
                 $("#button-generator-div").show();
-                $("#button-result").css("font-size", $("#font-size-property")[0].value + "px");
-                $("#button-result").css("border-radius", $("#border-radius-property")[0].value + "px");
-                $("#button-result").css("padding", $("#padding-property")[0].value + "px");
-                $("#button-result").css("background-color", $("#background-color-property")[0].value);
-                $("#button-result").css("color", $("#font-color-property")[0].value);
-                $("#button-result").css("box-shadow", $("#horizontal-shadow-property")[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $("#color-shadow-property")[0].value);
-                $("#button-result").css("border", $("#border-size-property")[0].value + "px solid " + $("#border-color-property")[0].value);
-                var text_css_code = "font-size:" + $("#font-size-property")[0].value + "px";
+                $("#button-generator-div #button-result").css("font-size", $("#button-generator-div #font-size-property")[0].value + "px");
+                $("#button-generator-div #button-result").css("border-radius", $("#button-generator-div #border-radius-property")[0].value + "px");
+                $("#button-generator-div #button-result").css("padding", $("#button-generator-div #padding-property")[0].value + "px");
+                $("#button-generator-div #button-result").css("background-color", $("#button-generator-div #background-color-property")[0].value);
+                $("#button-generator-div #button-result").css("color", $("#button-generator-div #font-color-property")[0].value);
+                $("#button-generator-div #button-result").css("box-shadow", $("#button-generator-div #horizontal-shadow-property")[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $("#color-shadow-property")[0].value);
+                $("#button-generator-div #button-result").css("border", $("#button-generator-div #border-size-property")[0].value + "px solid " + $("#border-color-property")[0].value);
+                var text_css_code = "font-size:" + $("#button-generator-div #font-size-property")[0].value + "px";
                 text_css_code += "\n";
-                text_css_code += "border-radius:" + $("#border-radius-property")[0].value + "px";
+                text_css_code += "border-radius:" + $("#button-generator-div #border-radius-property")[0].value + "px";
                 text_css_code += "\n";
-                text_css_code += "backgorund-color:" + $("#background-color-property")[0].value;
+                text_css_code += "backgorund-color:" + $("#button-generator-div #background-color-property")[0].value;
                 text_css_code += "\n";
-                text_css_code += "color:" + $("#font-color-property")[0].value;
+                text_css_code += "color:" + $("#button-generator-div #font-color-property")[0].value;
                 text_css_code += "\n";
-                text_css_code += "border:" + $("#border-size-property")[0].value + "px solid " + $("#border-color-property")[0].value;
+                text_css_code += "border:" + $("#button-generator-div #border-size-property")[0].value + "px solid " + $("#button-generator-div #border-color-property")[0].value;
                 text_css_code += "\n";
-                text_css_code += "box-shadow:" + $("#horizontal-shadow-property")[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $("#color-shadow-property")[0].value;
+                text_css_code += "box-shadow:" + $("#button-generator-div #horizontal-shadow-property")[0].value + "px " + $("#button-generator-div #vertical-shadow-property")[0].value + "px " + $("#button-generator-div #blur-shadow-property")[0].value + "px " + $("#button-generator-div #color-shadow-property")[0].value;
                 text_css_code += "\n";
-                text_css_code += "padding:" + $("#padding-property")[0].value + "px";
+                text_css_code += "padding:" + $("#button-generator-div #padding-property")[0].value + "px";
                 $("#button-generator-div .css-code").text(text_css_code);
                 break;
             case "Menu generator":
                 $("#menu-generator-div").show();
+                $("#menu-generator-div #menu-result > li").css("border", $("#menu-generator-div #border-menu-size-property")[0].value + "px solid " + $("#menu-generator-div #border-menu-color-property")[0].value);
+                $("#menu-generator-div #menu-result > li").css("border-radius", $("#menu-generator-div #border-radius-menu-property")[0].value + "px");
+                $("#menu-generator-div #menu-result > li").css("font-size", $("#menu-generator-div #font-size-menu-property")[0].value + "px");
+                $("#menu-generator-div #menu-result > li").css("background-color", $("#menu-generator-div #background-color-menu-property")[0].value);
+                $("#menu-generator-div #menu-result > li").css("color", $("#menu-generator-div #font-color-menu-property")[0].value);
+
                 break;
         }
     });
@@ -50,53 +56,76 @@ $(document).ready(function() {
     $("#button-generator-div .lessVariable").change(function() {
         switch (this.id) {
             case "font-size-property":
-                $("#button-result").css("font-size", $(this)[0].value + "px");
+                $("#button-generator-div #button-result").css("font-size", $(this)[0].value + "px");
                 break;
             case "background-color-property":
-                $("#button-result").css("background-color", $(this)[0].value);
+                $("#button-generator-div #button-result").css("background-color", $(this)[0].value);
                 break;
             case "font-color-property":
-                $("#button-result").css("color", $(this)[0].value);
+                $("#button-generator-div #button-result").css("color", $(this)[0].value);
                 break;
             case "horizontal-shadow-property":
-                $("#button-result").css("box-shadow", $(this)[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $("#color-shadow-property")[0].value);
+                $("#button-generator-div #button-result").css("box-shadow", $(this)[0].value + "px " + $("#button-generator-div #vertical-shadow-property")[0].value + "px " + $("#button-generator-div #blur-shadow-property")[0].value + "px " + $("#button-generator-div #color-shadow-property")[0].value);
                 break;
             case "vertical-shadow-property":
-                $("#button-result").css("box-shadow", $("#horizontal-shadow-property")[0].value + "px " + $(this)[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $("#color-shadow-property")[0].value);
+                $("#button-result").css("box-shadow", $("#button-generator-div #horizontal-shadow-property")[0].value + "px " + $(this)[0].value + "px " + $("#button-generator-div #blur-shadow-property")[0].value + "px " + $("#button-generator-div #color-shadow-property")[0].value);
                 break;
             case "blur-shadow-property":
-                $("#button-result").css("box-shadow", $("#horizontal-shadow-property")[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $(this)[0].value + "px " + $("#color-shadow-property")[0].value);
+                $("#button-result").css("box-shadow", $("#button-generator-div #horizontal-shadow-property")[0].value + "px " + $("#button-generator-div #vertical-shadow-property")[0].value + "px " + $(this)[0].value + "px " + $("#button-generator-div #color-shadow-property")[0].value);
                 break;
             case "color-shadow-property":
-                $("#button-result").css("box-shadow", $("#horizontal-shadow-property")[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $(this)[0].value);
+                $("#button-result").css("box-shadow", $("#button-generator-div #horizontal-shadow-property")[0].value + "px " + $("#button-generator-div #vertical-shadow-property")[0].value + "px " + $("#button-generator-div #blur-shadow-property")[0].value + "px " + $(this)[0].value);
                 break;
             case "border-radius-property":
-                $("#button-result").css("border-radius", $(this)[0].value + "px");
+                $("#button-generator-div #button-result").css("border-radius", $(this)[0].value + "px");
                 break;
             case "border-size-property":
-                $("#button-result").css("border", $(this)[0].value + "px solid " + $("#border-color-property")[0].value);
+                $("#button-generator-div #button-result").css("border", $(this)[0].value + "px solid " + $("#button-generator-div #border-color-property")[0].value);
                 break;
             case "border-color-property":
-                $("#button-result").css("border", $("#border-size-property")[0].value + "px solid " + $(this)[0].value);
+                $("#button-generator-div #button-result").css("border", $("#button-generator-div #border-size-property")[0].value + "px solid " + $(this)[0].value);
                 break;
             case "padding-property":
-                $("#button-result").css("padding", $("#padding-property")[0].value + "px");
+                $("#button-generator-div #button-result").css("padding", $(this)[0].value + "px");
                 break;
         }
-        var text_css_code = "font-size:" + $("#font-size-property")[0].value + "px";
+        var text_css_code = "font-size:" + $("#button-generator-div #font-size-property")[0].value + "px";
         text_css_code += "\n";
-        text_css_code += "border-radius:" + $("#border-radius-property")[0].value + "px";
+        text_css_code += "border-radius:" + $("#button-generator-div #border-radius-property")[0].value + "px";
         text_css_code += "\n";
-        text_css_code += "backgorund-color:" + $("#background-color-property")[0].value;
+        text_css_code += "backgorund-color:" + $("#button-generator-div #background-color-property")[0].value;
         text_css_code += "\n";
-        text_css_code += "color:" + $("#font-color-property")[0].value;
+        text_css_code += "color:" + $("#button-generator-div #font-color-property")[0].value;
         text_css_code += "\n";
-        text_css_code += "border:" + $("#border-size-property")[0].value + "px solid " + $("#border-color-property")[0].value;
+        text_css_code += "border:" + $("#button-generator-div #border-size-property")[0].value + "px solid " + $("#button-generator-div #border-color-property")[0].value;
         text_css_code += "\n";
-        text_css_code += "box-shadow:" + $("#horizontal-shadow-property")[0].value + "px " + $("#vertical-shadow-property")[0].value + "px " + $("#blur-shadow-property")[0].value + "px " + $("#color-shadow-property")[0].value;
+        text_css_code += "box-shadow:" + $("#button-generator-div #horizontal-shadow-property")[0].value + "px " + $("#button-generator-div #vertical-shadow-property")[0].value + "px " + $("#button-generator-div #blur-shadow-property")[0].value + "px " + $("#button-generator-div #color-shadow-property")[0].value;
         text_css_code += "\n";
-        text_css_code += "padding:" + $("#padding-property")[0].value + "px";
+        text_css_code += "padding:" + $("#button-generator-div #padding-property")[0].value + "px";
         $("#button-generator-div .css-code").text(text_css_code);
+    });
+    $("#menu-generator-div .lessVariable").change(function() {
+        switch (this.id) {
+            case "border-menu-size-property":
+                $("#menu-generator-div #menu-result > li").css("border", $(this)[0].value + "px solid " + $("#menu-generator-div #border-menu-color-property")[0].value);
+                break;
+            case "border-menu-color-property":
+                $("#menu-generator-div #menu-result > li").css("border", $("#menu-generator-div #border-menu-size-property")[0].value + "px solid " + $(this)[0].value);
+                break;
+            case "border-radius-menu-property":
+                $("#menu-generator-div #menu-result > li").css("border-radius", $("#menu-generator-div #border-radius-menu-property")[0].value + "px");
+                break;
+            case "font-size-menu-property":
+                $("#menu-generator-div #menu-result > li").css("font-size", $("#menu-generator-div #font-size-menu-property")[0].value + "px");
+                break;
+            case "background-color-menu-property":
+                $("#menu-generator-div #menu-result > li").css("background-color", $(this)[0].value);
+                break;
+            case "font-color-menu-property":
+                $("#menu-generator-div #menu-result > li").css("color", $(this)[0].value);
+                break;
+
+        }
     });
 });
 
